ea7f9995cedfcfc8d658ef09bec36a2aca8e906c
Hamed Osama <77761661+HamedOsama@users.noreply.github.com>
2022-09-04 09:23:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const ServerError = require('./src/interface/Error');
 connectDatabase();
 
 const app = express();
-const port = config.port || 3000
+const port =  3000
 app.use(cors())
 app.use(express.json())
 
