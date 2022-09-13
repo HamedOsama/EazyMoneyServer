be88258bb05388e79933176d399d7c0e49681767
Hamed Osama <hamedosama19@gmail.com>
2022-09-13 15:39:57 +0200

    try again

--- src/utils/uploadPhoto.js
+++ src/utils/uploadPhoto.js
@@ -9,12 +9,13 @@ const Uploads = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       const fullPath = path.join(__dirname, '../uploads')
+      console.log(fullPath);
       cb(null, fullPath)
     },
     filename: (req, file, cb) => {
       console.log(req.body);
-      const fileName = Date.now().toString() + "" + file.originalname
-      cb(null, fileName)
+      const fileName = Date.now().toString() + "" + file.originalname + "";
+      cb(null, fileName);
     }
   }),
 })
