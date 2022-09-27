0a3c284837cf961c6bbf027af13f0906900583aa
Hamed Osama <hamedosama19@gmail.com>
2022-09-27 03:31:59 +0200

    update response shape of get all orders array

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1169,7 +1169,11 @@ const addMoreDataToOrder = async (orders) => {
     newOrderForm.OrderedProduct = product;
     newOrderForm.buyer = buyer;
     newOrderForm.seller = seller;
-    newOrderForm.OrderedProperties = el.orderItems.map(orderProperty => product.properties.find(property => property._id.toString() === orderProperty.propertyId.toString()))
+    newOrderForm.OrderedProperties = orders.orderItems.map(orderProperty => {
+      const propertiesNewForm = product.properties.find(property => property._id.toString() === orderProperty.propertyId.toString())
+      propertiesNewForm.amount = orderProperty.quantity;
+      return propertiesNewForm;
+    })
     newOrders.push(newOrderForm)
   }
   return newOrders;
